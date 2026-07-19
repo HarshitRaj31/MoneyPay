0d5fad6d38f6aa5990680f8db88d39c6858ed46b
Harshit Raj <iamharshit106@gmail.com>
2026-07-19 14:31:34 +0530

    Fix payment verification API

--- client/vite-project/src/components/PaymentSuccess.jsx
+++ client/vite-project/src/components/PaymentSuccess.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import axios from "axios";
 import { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 const PaymentSuccess = () => {
      const [search] = useSearchParams();
-
+const navigate = useNavigate();
     useEffect(() => {
 
         const verify = async () => {
@@ -24,7 +25,27 @@ const PaymentSuccess = () => {
     }, []);
   return (
     <>
-    <h1>Payment Successful ❤️</h1></>
+   <div className="payment-success">
+    <div className="success-card">
+      <div className="success-icon">✓</div>
+
+      <h1>Payment Successful</h1>
+
+      <p>
+        Thank you for your support ❤️
+        <br />
+        Your donation has been received successfully.
+      </p>
+
+      <button
+        className="dashboard-btn"
+        onClick={() => navigate("/dashboard")}
+      >
+        Return to Dashboard
+      </button>
+    </div>
+  </div></>
+    
   )
 }
 
